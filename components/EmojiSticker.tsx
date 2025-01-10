65286a06eaafe4eb46d5196b7bdfb0a5ff872721
RicardoNLuisMateos <ricardonluism@gmail.com>
2025-01-10 16:20:47 -0600

    Add a tap gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,7 @@
 import { View } from 'react-native';
-import { Image, type ImageSource } from 'expo-image';
+import { type ImageSource } from 'expo-image';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 type Props = {
   imageSize: number;
@@ -7,9 +9,37 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+    // Animated
+    const scaleImage = useSharedValue(imageSize);
+    // Detect double tap
+    const doubleTab = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart( () => {
+        // Scale
+        if(scaleImage.value !== imageSize * 2) {
+            // Resize to double
+            scaleImage.value = scaleImage.value * 2;
+        } else {
+            // Resize to original
+            scaleImage.value = Math.round(scaleImage.value / 2);
+        }
+    });
+    // Takes the size values ​​that are modified by double clicking
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+          width: withSpring(scaleImage.value),
+          height: withSpring(scaleImage.value),
+        };
+    });
   return (
     <View style={{ top: -350 }}>
-      <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+        <GestureDetector gesture={doubleTab}>
+            <Animated.Image
+                source={stickerSource}
+                resizeMode="contain"
+                style={[imageStyle, { width: imageSize, height: imageSize }]}
+            />
+        </GestureDetector>
     </View>
   );
 }
