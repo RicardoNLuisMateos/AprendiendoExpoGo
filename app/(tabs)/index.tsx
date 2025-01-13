e77eaec885fc4034e0986552f02b988f34f680ad
RicardoNLuisMateos <ricardonluism@gmail.com>
2025-01-13 06:04:50 -0600

    Handle platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState, useRef } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
 import { type ImageSource } from "expo-image";
+import domtoimage from "dom-to-image"; 
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -59,19 +60,38 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      // Get the local URI for the image
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-      // Save the image to the device's media library
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Saved!');
+
+    if(Platform.OS === 'web') {
+      try {
+         // @ts-ignore
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        // Get the local URI for the image
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        // Save the image to the device's media library
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
