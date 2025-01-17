706ad2b245fe2d865c7c59cf9bc236a503336ee0
RicardoNLuisMateos <ricardonluism@gmail.com>
2025-01-17 11:16:47 -0600

    Configure multiple app variants

---
+++ app.config.js
@@ -0,0 +1,78 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.ricardonluis.login1.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.ricardonluis.login1.preview';
+  }
+
+  return 'com.ricardonluis.login1';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+export default{
+  "expo": {
+    "name": getAppName(),
+    "slug": "login1",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "myapp",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "package": getUniqueIdentifier()
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff"
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    "extra": {
+      "router": {
+        "origin": false
+      },
+      "eas": {
+        "projectId": "cb608f5c-f188-43d5-a3f0-9eaf173d103f"
+      }
+    },
+    "owner": "ricardonluis"
+  }
+}
